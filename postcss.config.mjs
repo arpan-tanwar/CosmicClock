18a5dd8cb2abe97cec3d88a5eca7e1f2be03ff0f
Arpan Tanwar <arpantanwar.at@gmail.com>
2024-10-06 00:52:02 -0400

    Initial Commit

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,7 +1,23 @@
 /** @type {import('postcss-load-config').Config} */
 const config = {
   plugins: {
+    "postcss-import": {},
+    "tailwindcss/nesting": "postcss-nesting",
     tailwindcss: {},
+    autoprefixer: {},
+    "postcss-pxtorem": {
+      rootValue: 16,
+      unitPrecision: 5,
+      propWhiteList: [
+        "*",
+        "!border",
+        "!border-width",
+        "!border-top-width",
+        "!border-right-width",
+        "!border-bottom-width",
+        "!border-left-width",
+      ],
+    },
   },
 };
 
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,10 +7,29 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    animation: {
+      blink: "blink 1s linear infinite",
+    },
+    keyframes: {
+      blink: {
+        "0%": { opacity: 0 },
+        "50%": { opacity: 1 },
+        "100%": { opacity: 0 },
+      },
+    },
+    fontFamily: {
+      mono: ["ballingermono"],
+    },
     extend: {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        "almost-white": "#F7E2E4",
+        "almost-black": "#0d0618",
+        "spaceship-black": "#201112",
+        "space-purple": "#5B3CB3",
+        "cloud-pink": "#EE848D",
+        "pop-pink": "#ff7f91",
       },
     },
   },
