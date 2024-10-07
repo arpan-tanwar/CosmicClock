081c0e89265a8b9fe64739eb8cbf7e2a4fb77729
Arpan Tanwar <arpantanwar.at@gmail.com>
2024-10-06 23:52:09 -0400

    c

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,9 +12,9 @@ const config: Config = {
     },
     keyframes: {
       blink: {
-        "0%": { opacity: 0 },
-        "50%": { opacity: 1 },
-        "100%": { opacity: 0 },
+        "0%": { opacity: "0" },
+        "50%": { opacity: "1" },
+        "100%": { opacity: "0" },
       },
     },
     fontFamily: {
